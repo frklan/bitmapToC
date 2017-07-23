6b75081b949bad42f3b1542fa91dfd555b85a56e
Fredrik <fredrik.k.andersson@gmail.com>
2017-07-23 18:28:54 +0200

    Added ..function to select horizontal or vertical encoding ..edit-menu for copy keyboard shortcut ..possibility to select if output array to be prefixed with "PROGMEM" or not ..settings saved at exit/reloaded at start

--- Bitmapper/ViewController.swift
+++ Bitmapper/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: NSViewController, DropZoneDelegate {
     @IBOutlet var textView: NSTextView!
     @IBOutlet weak var dragHereLabel: NSTextField!
     @IBOutlet weak var bitsBox: NSComboBox!
+    @IBOutlet weak var checkUseProgmem: NSButton!
     
     var selectedImage: NSImage?
     
@@ -34,6 +35,21 @@ class ViewController: NSViewController, DropZoneDelegate {
         // setup textView
         textView.font = NSFont(name: "Menlo", size: 11)
         
+        let defaults = UserDefaults.standard
+        checkUseProgmem.state = defaults.integer(forKey: "Use Progmem")
+        bitsBox.integerValue = defaults.integer(forKey: "Bits per Pixels")
+    }
+    
+    override func viewWillDisappear()
+    {
+        let defaults = UserDefaults.standard
+      
+        defaults.set(checkUseProgmem.state, forKey: "Use Progmem")
+        defaults.set(bitsBox.integerValue, forKey: "Bits per Pixels")
+        
+        // Use this to clear the defaults..
+        //UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        //UserDefaults.standard.synchronize()
     }
     
     override func viewDidDisappear()
@@ -81,7 +97,7 @@ class ViewController: NSViewController, DropZoneDelegate {
         
     }
     
-    @IBAction func convertBitmapToC(_ sender: AnyObject) {
+    @IBAction func convertBitmapToCHorizontal(_ sender: AnyObject) {
         
         if let img = selectedImage {
             
@@ -89,14 +105,14 @@ class ViewController: NSViewController, DropZoneDelegate {
             let rep = NSBitmapImageRep(data: img.tiffRepresentation!)!
             let height = Int(img.size.height)
             let width = Int(img.size.width)
+            let progmem = (checkUseProgmem.state == NSOnState ? "PROGMEM " : "")
         
             // amount of pixel values in one array item
             let bitsPerItem = bitsBox.integerValue
             if bitsPerItem == 0 { return }
             
             // output string
-            //var output = "// width: \(width) height: \(height)\nPROGMEM const unsigned char YOUR_NAME[] = {\n\(width), \(height),"
-            var output = "// width: \(width) height: \(height)\nPROGMEM const unsigned char YOUR_NAME[] = {"
+            var output = "// width: \(width) height: \(height) (Horizontal encoding)\n\(progmem)const unsigned char YOUR_NAME[] = {"
             
             // y loop
             for row in 0 ..< height {
@@ -145,6 +161,141 @@ class ViewController: NSViewController, DropZoneDelegate {
         }
         
     }
+    
+    @IBAction func convertBitmapToCVertical(_ sender: AnyObject) {
+        
+        if let img = selectedImage {
+            
+            // get neccesary data: bitmaprep & dimensions
+            let rep = NSBitmapImageRep(data: img.tiffRepresentation!)!
+            let height = Int(img.size.height)
+            let width = Int(img.size.width)
+            let progmem = (checkUseProgmem.state == NSOnState ? "PROGMEM " : "")
+            
+            // amount of pixel values in one array item
+            let bitsPerItem = bitsBox.integerValue
+            if bitsPerItem == 0 { return }
+            
+            // output string
+            var output = "// width: \(width) height: \(height) (Vertical encoding)\n\(progmem)const unsigned char YOUR_NAME[] = {"
+            
+            // y loop
+            for col in 0 ..< width {
+                
+                output += "\n" // add line break
+                var currentValue = 0 // the current array item
+                var posInCurrentValue = 0 // current bit in current array item
+                
+                // x loop
+                for row in 0 ..< height {
+                    
+                    // add 1 or 0 at correct position in currentValue
+                    let lsl = bitsPerItem - posInCurrentValue - 1
+                    let bit = valueForColor(rep.colorAt(x: col, y: row)!) << lsl
+                    currentValue += bit
+                    
+                    
+                    posInCurrentValue += 1
+                    
+                    if posInCurrentValue == bitsPerItem {
+                        
+                        // add to output string as hexadecimal with at least two digits, it still looks messy with more than 8 bits per array item
+                        let valueString = String(currentValue, radix: 16)
+                        output +=  "0x\(valueString.leftPadding(toLength: 2, withPad: "0"))"
+                        
+                        // if it isn't the last object, add a comma
+                        if row != height-1 || col != width-1 { output += ", " }
+                        
+                        // reset
+                        currentValue = 0
+                        posInCurrentValue = 0
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            
+            // finish array
+            output += "\n\n};"
+            
+            // show it to the user
+            textView.string = output
+            
+        }
+        
+    }
+    
+    
+/*    @IBAction func convertBitmapToCVertical(_ sender: AnyObject) {
+        
+        if let img = selectedImage {
+            
+            // amount of pixel values in one array item
+            let bitsPerItem = bitsBox.integerValue
+            if bitsPerItem == 0 { return }
+            
+            // get neccesary data: bitmaprep & dimensions
+            let rep = NSBitmapImageRep(data: img.tiffRepresentation!)!
+            let bmpHeight = Int(img.size.height)
+            let bmpWidth = Int(img.size.width)
+            let arrayHeight = Int(ceil(Double(bmpHeight) / Double(bitsPerItem))) * bitsPerItem
+
+            let arrayWidth = Int(ceil(Double(bmpWidth) / Double(bitsPerItem))) * bitsPerItem
+            
+            // output string
+            var output = "// width: \(bmpWidth) height: \(bmpHeight) (vertical encoding)\nPROGMEM const unsigned char YOUR_NAME[] = {"
+            
+            // y loop
+            for col in 0 ..< arrayWidth {
+                
+                output += "\n" // add line break
+                var currentValue = 0 // the current array item
+                var posInCurrentValue = 0 // current bit in current array item
+                
+                // x loop
+                for row in 0 ..< arrayHeight {
+                    
+                    // add 1 or 0 at correct position in currentValue
+                    let lsl = bitsPerItem - posInCurrentValue - 1
+                    var bit = 0
+                    if(col < bmpWidth && row < bmpHeight)
+                    {
+                        bit = valueForColor(rep.colorAt(x: col, y: row)!) << lsl
+                    }
+                    currentValue += bit
+                    
+                    
+                    posInCurrentValue += 1
+                    
+                    if posInCurrentValue == bitsPerItem {
+                        
+                        // add to output string as hexadecimal with at least two digits, it still looks messy with more than 8 bits per array item
+                        let valueString = String(currentValue, radix: 16)
+                        output +=  "0x\(valueString.leftPadding(toLength: 2, withPad: "0"))"
+                        
+                        // if it isn't the last object, add a comma
+                        if row != arrayHeight - 1 || col != arrayWidth - 1 { output += ", " }
+                        
+                        // reset
+                        currentValue = 0
+                        posInCurrentValue = 0
+                    }
+                }
+                
+            }
+            
+            // finish array
+            output += "\n\n};"
+            
+            // show it to the user
+            textView.string = output
+            
+        }
+        
+    }
+*/
 
     @IBAction func openFile(_ sender: AnyObject) {
         
